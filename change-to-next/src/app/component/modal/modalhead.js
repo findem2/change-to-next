a96010afb8bcdda95f8809265e66ae733ce4f0e2
findem2 <thsalsqhr@gmail.com>
2024-09-09 11:56:29 +0900

    error fix

--- change-to-next/src/app/component/modal/modalhead.js
+++ change-to-next/src/app/component/modal/modalhead.js
@@ -1,8 +1,11 @@
 // components/Modal.js
 import { useState } from "react";
+import Link from "next/link";
 
 const Modalhead = () => {
   const [isOpen, setIsOpen] = useState(false);
+  let link = "http://localhost:3000";
+  let name = "손";
 
   return (
     <div>
@@ -15,14 +18,33 @@ const Modalhead = () => {
           <div className="bg-white w-96 h-[95%] rounded-lg p-6 relative">
             <button
               onClick={() => setIsOpen(false)}
-              className="absolute top-3 left-3 text-black"
+              className="absolute bottom-[105%] right-[45%] text-black px-5 py-2 h-16 w-16 rounded-full bg-white"
             >
-              ←
+              ✕
             </button>
             <div className="mt-8">
-              <h2 className="text-lg font-bold mb-4">메뉴</h2>
+              <button onClick={() => setIsOpen(false)} className=" text-black">
+                ← Headphones
+              </button>
               <ul className="space-y-4">
-                <li className="font-semibold">헤드폰</li>
+                <li className="flex">
+                  <div>
+                    <img
+                      className=""
+                      src="/img/hamburger/MW75S1-PDP_1350x1350_Hero_V2_1a4bd298-a68f-47ca-b050-b3cc3f47283e.webp"
+                    ></img>
+                    <div>MW75</div>
+                    <Link href={link}> 복{name}</Link>
+                  </div>
+                  <div>
+                    <img
+                      className=""
+                      src="/img/hamburger/MH40G4-W2-PDP_1350x1350_Hero_V1_06e56a75-704e-47e6-a742-ef7a3709fefd.webp"
+                    ></img>
+                    <div>MH40 WIRELESS</div>
+                    <div></div>
+                  </div>
+                </li>
                 <li className="font-semibold">이어폰</li>
                 <li className="font-semibold">액세서리</li>
                 <li className="font-semibold">콜라보레이션</li>
---
+++ change-to-next/src/app/product/page.js
@@ -0,0 +1,11 @@
+import Footer from "../component/footer/footer";
+import MySwiper from "../component/swifer/myswiper";
+
+export default function Product() {
+  return (
+    <div>
+      <MySwiper />
+      <Footer />
+    </div>
+  );
+}
